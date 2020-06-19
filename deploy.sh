c2e956abc2a97b931520a8b7795ebd1452ae32a6
adai <adai_mail@126.com>
2020-06-19 16:36:26 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PROJECT_HOME = /home/test/website
+ENV_BIN =  /home/test/for_website/bin
+cd $PROJECT_HOME
+git pull
+
+$ENV_BIN/pip install -r requirements.txt
+$ENV_BIN/python3 manage.py collectstatic --noinput
+$ENV_BIN/python3 manage.py makemigrations
+$ENV_BIN/python3 manage.py migrate
+
+killall -9 uwsgi
+
+exit 0
